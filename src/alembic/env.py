b102375c035cd3e4f6b32b4a76d60743a600e967
Eugene Vorontsov <sejeenn@gmail.com>
2024-11-06 21:43:58 +0300

    забыл прописать Base... исправил

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -7,7 +7,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from src.core.config import settings
+from core.config import settings
+from core.models.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
