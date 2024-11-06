02742078fd029d8ccac16f4d0cc8db54283b010b
Eugene Vorontsov <sejeenn@gmail.com>
2024-11-06 21:29:22 +0300

    раскомментировал src/core/models/__init__.py

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,7 +1,7 @@
-# __all__ = (
-#     "db_helper",
-#     "Base",
-# )
-#
-# from .db_helper import db_helper
-# from .base import Base
+__all__ = (
+    "db_helper",
+    "Base",
+)
+
+from .db_helper import db_helper
+from .base import Base
