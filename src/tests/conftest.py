dd5dcb760d90a81de9646c3f6910c137cc028bec
Eugene Vorontsov <sejeenn@gmail.com>
2024-11-11 00:37:40 +0300

    Добавлены pytest-asyncio, trio. Сделаны настройки asyncio_default_fixture_loop_scope. Тесты пока не работают. Всё ломается при обращении к БД.

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,16 +1,29 @@
-import os
-import pytest
-from main import main_app
-from httpx import AsyncClient, ASGITransport
-
-
-os.environ['ENV'] = 'testing'
-
-
-@pytest.fixture
-async def async_client():
-    async with AsyncClient(
-            transport=ASGITransport(app=main_app),
-            base_url="http://localhost:8000"
-    ) as async_client:
-        yield async_client
+# from typing import Annotated
+#
+# import pytest_asyncio
+# from fastapi import Depends
+# from httpx import AsyncClient, ASGITransport
+# from collections.abc import AsyncGenerator
+#
+# from sqlalchemy.ext.asyncio import AsyncSession
+#
+# from core.models import db_helper
+# from main import main_app
+#
+#
+# @pytest_asyncio.fixture(loop_scope='session', scope="session")
+# async def async_client() -> AsyncGenerator[AsyncClient, None]:
+#     async with AsyncClient(
+#             transport=ASGITransport(app=main_app), base_url='http://test'
+#     ) as async_test_client:
+#         yield async_test_client
+# #
+# #
+# # @pytest_asyncio.fixture(scope="session")
+# # async def async_db(
+# #         session: Annotated[
+# #             AsyncSession,
+# #             Depends(db_helper.session_getter),
+# #         ],
+# # ):
+# #     yield session
--- src/tests/test_temp.py
+++ src/tests/test_temp.py
@@ -1,2 +1,28 @@
-def test_any():
-    assert 1 == 1
+import os
+from dotenv import load_dotenv
+import pytest
+import pytest_asyncio
+import asyncio
+from httpx import AsyncClient
+# from .conftest import async_client
+
+
+dot_env = load_dotenv("../.test.env")
+
+
+# @pytest.mark.asyncio(loop_scope="session")
+# async def test_register(async_client: AsyncClient):
+#     print("++++++++++++++++++++", dot_env)
+#     print("====event_loop====", asyncio.get_event_loop())
+#     response = await async_client.get("/")
+#     assert response.status_code == 200
+
+
+@pytest_asyncio.fixture(loop_scope="module")
+async def current_loop():
+    return asyncio.get_running_loop()
+
+
+@pytest.mark.asyncio(loop_scope="module")
+async def test_runs_in_module_loop(current_loop):
+    assert current_loop is asyncio.get_running_loop()
