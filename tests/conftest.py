6a5a8df10e15f252f82b1ee57f3c010e07bc6e59
Евгений Воронцов <sejeenn@gmail.com>
2024-11-12 16:15:18 +0300

    Переписаны заново все импорты после того как неудачно их все обнулил

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_asyn
 from sqlalchemy.pool import NullPool
 
 from src.core.models.base import Base
-from src.core.models.db_helper import db_helper
+from src.core.models import db_helper
 from src.core.models.ingredient import Ingredient
 from src.core.models.ingredient_in_recipe import IngredientsInRecipe
 from src.core.models.recipe import Recipe
