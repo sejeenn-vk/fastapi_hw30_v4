a112a6ef888a66a4403997631d0239acda001d7f
Eugene Vorontsov <sejeenn@gmail.com>
2024-11-12 20:17:57 +0300

    Исправлен файл создания пользователя test базы данных, чтобы пользователь был владельцем базы данных test

--- init-database.sh
+++ init-database.sh
@@ -4,6 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER test WITH PASSWORD 'test';
     CREATE DATABASE test;
-    GRANT ALL PRIVILEGES ON DATABASE test TO test;
-
+    ALTER DATABASE test OWNER TO test;
 EOSQL
